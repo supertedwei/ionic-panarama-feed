6b5cf11388fb0bc693fbedeb6594c347c377cc81
Ted Wei <jdwei@hotmail.com>
2017-01-08 06:25:35 +0800

    sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PhotoTiltComponent } from '../components/photo-tilt/photo-tilt';
+import { ParallaxHeader } from '../components/parallax-header/parallax-header';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PhotoTiltComponent,
+    ParallaxHeader
   ],
   imports: [
     IonicModule.forRoot(MyApp)
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
-
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-    
-  }
-
+ 
+    cards: any;
+    category: string = 'gear';
+ 
+    constructor(public navCtrl: NavController) {
+ 
+        this.cards = new Array(10);
+ 
+    }
+ 
 }
